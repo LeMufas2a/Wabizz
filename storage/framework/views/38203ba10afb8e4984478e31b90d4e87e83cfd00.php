661c30691d1b7acb554dfe174b61ad3625e09acc
Mohammed Akhtar <codage.mufasa@gmail.com>
2021-12-26 22:52:04 +0100

    storage

---
+++ storage/framework/views/38203ba10afb8e4984478e31b90d4e87e83cfd00.php
@@ -0,0 +1,5 @@
+ <!-- CSS Files -->
+ <?php if(in_array(config('app.locale'),['ar','he','fa','ur'])): ?>
+    <link href="<?php echo e(asset('css')); ?>/rtl.css" rel="stylesheet" />
+ <?php endif; ?>
+<?php /**PATH C:\wamp64\www\resources\views/layouts/rtl.blade.php ENDPATH**/ ?>
